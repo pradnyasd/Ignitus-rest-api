b9462e7f440527cfa286bc074b5a635350aa9d41
Pradnya <pradnya24.dalvi@gmail.com>
2019-02-27 17:45:30 +0530

    skip unnecessary test

--- config/db.spec.js
+++ config/db.spec.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 
 describe('Array -- This is a specimen. Please add a valid test', () => {
   describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', () => {
+    it.skip('should return -1 when the value is not present', () => {
       assert.equal([1, 2, 3, 4].indexOf(5), -1);
     });
   });
